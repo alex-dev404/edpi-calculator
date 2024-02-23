acf810cece768fa9f71f2b18660b78ca200ef105
alex-dev404 <158501300+alex-dev404@users.noreply.github.com>
2024-02-23 11:52:20 -0300

    programa base

---
+++ build/CMakeFiles/Sensi_Calculator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sensi_Calculator.dir/main.cpp.o"
+  "CMakeFiles/Sensi_Calculator.dir/main.cpp.o.d"
+  "Sensi_Calculator"
+  "Sensi_Calculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sensi_Calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Sensi_Calculator.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Sensi_Calculator.
+# This may be replaced when dependencies are built.
